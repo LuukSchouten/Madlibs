f3ed8386633c72ac14f2d2c39a7ae3d08ceba4cc
Otakufy <Luukschouten01@gmail.com>
2021-03-03 10:16:06 +0100

    finished madlibs
    -

--- Onkunde.php
+++ Onkunde.php
@@ -1,3 +1,98 @@
+<?php 
+
+$kunnenErr = $persoonErr = $getalErr = $vakantieErr = $eigenschapgoedErr = $eigenschapslechtErr = $overkomenErr = "";
+$kunnen = $persoon = $getal = $vakantie = $eigenschapgoed = $eigenschapslecht = $overkomen = "";
+$validated = true;
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    if (empty($_POST["kunnen"])) {
+      $kunnenErr = "Vul iets in";
+      $validated = false;
+    } else {
+      $kunnen = test_input($_POST["kunnen"]);
+      if (!preg_match("/^[a-zA-Z-' ]*$/",$kunnen)) {
+        $kunnenErr = "Alleen letters en witregels toegestaan";
+        $validated = false;
+      }
+    }
+  
+    if (empty($_POST["persoon"])) {
+      $persoonErr = "Vul iets in";
+      $validated = false;
+    } else {
+      $persoon = test_input($_POST["persoon"]);
+      if (!preg_match("/^[a-zA-Z-' ]*$/",$persoon)) {
+        $persoonErr = "Alleen letters en witregels toegestaan";
+        $validated = false;
+      }
+    }
+  
+    if (empty($_POST["getal"])) {
+      $getalErr = "Vul iets in";
+      $validated = false;
+    } else {
+      $getal = test_input($_POST["getal"]);
+      if (!preg_match("/^[a-zA-Z-' ]*$/",$getal)) {
+        $getalErr = "Alleen letters en witregels toegestaan";
+        $validated = false;
+      }
+    }
+  
+    if (empty($_POST["vakantie"])) {
+      $vakantieErr = "Vul iets in";
+      $validated = false;
+    } else {
+      $vakantie = test_input($_POST["vakantie"]);
+      if (!preg_match("/^[a-zA-Z-' ]*$/",$vakantie)) {
+        $vakantieErr = "Alleen letters en witregels toegestaan";
+        $validated = false;
+      }
+    }
+  
+    if (empty($_POST["eigenschapgoed"])) {
+      $eigenschapgoedErr = "Vul iets in";
+      $validated = false;
+    } else {
+      $eigenschapgoed = test_input($_POST["eigenschapgoed"]);
+      if (!preg_match("/^[a-zA-Z-' ]*$/",$eigenschapgoed)) {
+        $eigenschapgoedErr = "Alleen letters en witregels toegestaan";
+        $validated = false;
+      }
+    }
+
+    if (empty($_POST["eigenschapslecht"])) {
+        $eigenschapslechtErr = "Vul iets in";
+        $validated = false;
+      } else {
+        $eigenschapslecht = test_input($_POST["eigenschapslecht"]);
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$eigenschapslecht)) {
+          $eigenschapslechtErr = "Alleen letters en witregels toegestaan";
+          $validated = false;
+        }
+      }
+
+    if (empty($_POST["overkomen"])) {
+        $overkomenErr = "Vul iets in";
+        $validated = false;
+    } else {
+        $overkomen = test_input($_POST["overkomen"]);
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$overkomen)) {
+          $overkomen = "Alleen letters en witregels toegestaan";
+          $validated = false;
+        }
+    }
+  }
+  
+  
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,44 +118,57 @@
     <div class="middle">
         <div class="wrapper" name="wrap">
             <h1>Onkunde</h1>
+
+          <?php if ($_SERVER["REQUEST_METHOD"] == "POST" and $validated == true){?>
+
+            <style> .formpie{display: none;}</style>
+            Er zijn veel mensen die niet kunnen <?php echo $_POST["kunnen"];?>, 
+            Neem nou <?php echo $_POST["persoon"];?>, Zelfs met de hulp van een <?php echo $_POST["vakantie"];?> of zelfs 
+            <?php echo $_POST["getal"];?> kan <?php echo $_POST["persoon"];?> niet <?php echo $_POST["kunnen"];?>. Dat heeft niet te maken met 
+            een gebrek aan <?php echo $_POST["eigenschapgoed"];?> maar met een te veel aan <?php echo $_POST["eigenschapslecht"];?>.
+            Te veel <?php echo $_POST["eigenschapslecht"];?> leid tot <?php echo $_POST["overkomen"];?> en dat is niet goed als je wilt
+            <?php echo $_POST["kunnen"];?>. Helaas voor <?php echo $_POST["persoon"];?>.
+          
+          <?php } ?>
+          
+            <div class="formpie">
             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
                 <div class="formdivs">
                     <label for="text">Wat zou je graag willen kunnen?</label>
-                    <input type="text" name="kunnen" class="inputstuff"/>
+                    <input type="text" name="kunnen" class="inputstuff" value="<?php echo $kunnen;?>"/>
                     <span class="error">* <?php echo $kunnenErr;?></span>
                 </div>
                 <div class="formdivs">
                     <label for="text">Met welke persoon kun je goed opschieten?</label>
-                    <input type="text" name="persoon" class="inputstuff"/>
+                    <input type="text" name="persoon" class="inputstuff" value="<?php echo $persoon;?>"/>
                     <span class="error">* <?php echo $persoonErr;?></span>
                     
                 </div>
                 <div class="formdivs">
                     <label for="text">Wat is je favoriete getal?</label>
-                    <input type="text" name="getal" class="inputstuff"/>
+                    <input type="text" name="getal" class="inputstuff" value="<?php echo $getal;?>"/>
                     <span class="error">* <?php echo $getalErr;?></span>
-                  
                 </div>
                 <div class="formdivs">
                     <label for="text">Wat heb je altijd bij je als je op vakantie gaat?</label>
-                    <input type="text" name="vakantie" class="inputstuff"/>
-                   
+                    <input type="text" name="vakantie" class="inputstuff" value="<?php echo $vakantie;?>"/>
+                    <span class="error">* <?php echo $vakantieErr;?></span>
                 </div>
                 <div class="formdivs">
                     <label for="text">Wat is je beste persoonlijke eigenschap?</label>
-                    <input type="text" name="eigenschapgoed" class="inputstuff"/>
+                    <input type="text" name="eigenschapgoed" class="inputstuff" value="<?php echo $eigenschapgoed;?>"/>
                     <span class="error">* <?php echo $eigenschapgoedErr;?></span>
                   
                 </div>
                 <div class="formdivs">
                     <label for="text">Wat is je slechtste persoonlijke eigenschap?</label>
-                    <input type="text" name="eigenschapslecht" class="inputstuff"/>
+                    <input type="text" name="eigenschapslecht" class="inputstuff" value="<?php echo $eigenschapslecht;?>"/>
                     <span class="error">* <?php echo $eigenschapslechtErr;?></span>
                 
                 </div>
                 <div class="formdivs">
                     <label for="text">Wat is het ergste dat je kan overkomen?</label>
-                    <input type="text" name="overkomen" class="inputstuff"/>
+                    <input type="text" name="overkomen" class="inputstuff" value="<?php echo $overkomen;?>"/>
                     <span class="error">* <?php echo $overkomenErr;?></span>
                    
                 </div>
@@ -68,6 +176,7 @@
                     <input type="submit" value="Submit" class="submitbtn">
                 </div>
             </form>
+            </div>
         </div>
     </div>
 
@@ -77,71 +186,8 @@
 
 </div>
 
-<?php 
 
-$kunnenErr = $persoonErr = $getalErr = $vakantieErr = $eigenschapgoedErr = $eigenschapslechtErr = $overkomenErr = "";
-$kunnen = $persoon = $getal = $vakantie = $eigenschapgoed = $eigenschapslecht = $overkomen = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") { ?>
-
-  Er zijn veel mensen die niet kunnen <?php echo $_POST["kunnen"];?>, 
-  Neem nou <?php echo $_POST["persoon"];?>, Zelfs met de hulp van een <?php echo $_POST["vakantie"];?> of zelfs 
-  <?php echo $_POST["getal"];?> kan <?php echo $_POST["persoon"];?> niet <?php echo $_POST["kunnen"];?>. Dat heeft niet te maken met 
-  een gebrek aan <?php echo $_POST["eigenschapgoed"];?> maar met een te veel aan <?php echo $_POST["eigenschapslecht"];?>.
-  Te veel <?php echo $_POST["eigenschapslecht"];?> leid tot <?php echo $_POST["overkomen"];?> en dat is niet goed als je wilt
-  <?php echo $_POST["kunnen"];?>. Helaas voor <?php echo $_POST["persoon"];?>. <?php } 
-
-  /*
-    if (empty($_POST["kunnen"])) {
-      $kunnenErr = "Name is required";
-    } else {
-      $kunnen = test_input($_POST["kunnen"]);
-    }
-  
-    if (empty($_POST["persoon"])) {
-      $persoonErr = "Email is required";
-    } else {
-      $persoon = test_input($_POST["persoon"]);
-    }
-  
-    if (empty($_POST["getal"])) {
-      $getal = "";
-    } else {
-      $getal = test_input($_POST["getal"]);
-    }
-  
-    if (empty($_POST["vakantie"])) {
-      $vakantie = "";
-    } else {
-      $vakantie = test_input($_POST["vakantie"]);
-    }
-  
-    if (empty($_POST["eigenschapgoed"])) {
-      $eigenschapgoedErr = "Gender is required";
-    } else {
-      $eigenschapgoed = test_input($_POST["eigenschapgoed"]);
-    }
-
-    if (empty($_POST["eigenschapslecht"])) {
-        $eigenschapslechtErr = "Gender is required";
-      } else {
-        $eigenschapslecht = test_input($_POST["eigenschapslecht"]);
-      }
-
-    if (empty($_POST["overkomen"])) {
-        $overkomenErr = "Gender is required";
-    } else {
-        $overkomen = test_input($_POST["overkomen"]);
-    }
-  }
-  
-function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}*/
-?>
 
 </body>
 </html>
--- Paniek.php
+++ Paniek.php
@@ -1,3 +1,108 @@
+<?php 
+
+$huisdierErr = $persoonErr = $landErr = $verveeltErr = $speelgoedErr = $spijbelErr = $geldErr = $bezigheidErr = "";
+$huisdier = $persoon = $land = $verveelt = $speelgoed = $spijbel = $geld = $bezigheid = "";
+$validated = true;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    if (empty($_POST["huisdier"])) {
+      $huisdierErr = "Vul iets in";
+      $validated = false;
+    } else {
+      $huisdier = test_input($_POST["huisdier"]);
+      if (!preg_match("/^[a-zA-Z-' ]*$/",$huisdier)) {
+        $huisdierErr = "Alleen letters en witregels toegestaan";
+        $validated = false;
+      }
+    }
+  
+    if (empty($_POST["persoon"])) {
+      $persoonErr = "Vul iets in";
+      $validated = false;
+    } else {
+      $persoon = test_input($_POST["persoon"]);
+      if (!preg_match("/^[a-zA-Z-' ]*$/",$persoon)) {
+        $persoonErr = "Alleen letters en witregels toegestaan";
+        $validated = false;
+      }
+    }
+  
+    if (empty($_POST["land"])) {
+      $landErr = "Vul iets in";
+      $validated = false;
+    } else {
+      $land = test_input($_POST["land"]);
+      if (!preg_match("/^[a-zA-Z-' ]*$/",$land)) {
+        $landErr = "Alleen letters en witregels toegestaan";
+        $validated = false;
+      }
+    }
+  
+    if (empty($_POST["verveelt"])) {
+      $verveeltErr = "Vul iets in";
+      $validated = false;
+    } else {
+      $verveelt = test_input($_POST["verveelt"]);
+      if (!preg_match("/^[a-zA-Z-' ]*$/",$verveelt)) {
+        $verveeltErr = "Alleen letters en witregels toegestaan";
+        $validated = false;
+      }
+    }
+  
+    if (empty($_POST["speelgoed"])) {
+      $speelgoedErr = "Vul iets in";
+      $validated = false;
+    } else {
+      $speelgoed = test_input($_POST["speelgoed"]);
+      if (!preg_match("/^[a-zA-Z-' ]*$/",$speelgoed)) {
+        $speelgoedErr = "Alleen letters en witregels toegestaan";
+        $validated = false;
+      }
+    }
+
+    if (empty($_POST["spijbel"])) {
+        $spijbelErr = "Vul iets in";
+        $validated = false;
+    } else {
+        $spijbel = test_input($_POST["spijbel"]);
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$spijbel)) {
+          $spijbelErr = "Alleen letters en witregels toegestaan";
+          $validated = false;
+        }
+    }
+
+    if (empty($_POST["geld"])) {
+        $geldErr = "Vul iets in";
+        $validated = false;
+    } else {
+        $geld = test_input($_POST["geld"]);
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$geld)) {
+          $geldErr = "Alleen letters en witregels toegestaan";
+          $validated = false;
+        }
+    }
+
+    if (empty($_POST["bezigheid"])) {
+        $bezigheidErr = "Vul iets in";
+        $validated = false;
+    } else {
+        $bezigheid = test_input($_POST["bezigheid"]);
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$bezigheid)) {
+          $bezigheidErr = "Alleen letters en witregels toegestaan";
+          $validated = false;
+        }
+    }
+}
+  
+  
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,52 +127,90 @@
     
     <div class="middle">
         <div class="wrapper">
-            <h1>Er heerst paniek...</h1>
-            <form action="<?php echo htmlspecialchars($_SERVER["Paniekresultaat.php"]);?>" method="post">
+        <h1>Er heerst paniek...</h1>
+        
+        <?php if ($_SERVER["REQUEST_METHOD"] == "POST" and $validated == true){?>
+          <style> .formpie{display: none;}</style>
+          Er heerst paniek in het koningkrijk <?php echo $_POST["land"];?>. Koning <?php echo $_POST["spijbel"];?> is ten einde raad en als koning
+          </br>
+          <?php echo $_POST["spijbel"];?> ten einde raad is, dan roept hij zijn ten-einde-raadsheer <?php echo $_POST["persoon"];?>.
+          </br>
+          </br>
+          "<?php echo $_POST["persoon"];?> Het is een ramp! Het is een schande!"
+          </br>
+          </br>
+          "Sire, Majesteit, Uwe luidruchtigheid, wat is er aan de hand?"
+          </br>
+          </br>
+          "Mijn <?php echo $_POST["huisdier"];?> is verdwenen! Zo maar, zonder waarschuwing. En ik had net <?php echo $_POST["speelgoed"];?> voor hem 
+          </br>
+          gekocht!"
+          </br>
+          </br>
+          "Majesteit, uw <?php echo $_POST["huisdier"];?> komt vast vanzelf weer terug?"
+          </br>
+          </br>
+          "Ja, da's leuk en aardig, maar hoe moet ik in de tussentijd <?php echo $_POST["bezigheid"];?> leren?"
+          </br>
+          </br>
+          "Maar Sire, daar kunt u toch uw <?php echo $_POST["geld"];?> voor gebruiken?"
+          </br>
+          </br>
+          "<?php echo $_POST["persoon"];?>, je hebt helemaal gelijk!, Wat zou ik doen als ik jou niet had"
+          </br>
+          </br>
+          "<?php echo $_POST["verveelt"];?>, Sire"
+
+          
+        <?php } ?>
+
+            <div class="formpie">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
                 <div class="formdivs">
                     <label for="text">Welk dier zou je nooit als huisdier willen hebben?</label>
-                    <input type="text" name="huisdier" class="inputstuff"/>
-                    <span class="error">* <?php echo $nameErr;?></span>
+                    <input type="text" name="huisdier" class="inputstuff" value="<?php echo $huisdier;?>"/>
+                    <span class="error">* <?php echo $huisdierErr;?></span>
                 </div>
                 <div class="formdivs">
                     <label for="text">Wie is de belangrijkste persoon in je leven?</label>
-                    <input type="text" name="persoon" class="inputstuff"/>
-                    <span class="error">* <?php echo $nameErr;?></span>
+                    <input type="text" name="persoon" class="inputstuff" value="<?php echo $persoon;?>"/>
+                    <span class="error">* <?php echo $persoonErr;?></span>
                 </div>
                 <div class="formdivs">
                     <label for="text">In welk land zou je graag willen wonen?</label>
-                    <input type="text" name="land" class="inputstuff"/>
-                    <span class="error">* <?php echo $nameErr;?></span>
+                    <input type="text" name="land" class="inputstuff" value="<?php echo $land;?>"/>
+                    <span class="error">* <?php echo $landErr;?></span>
                 </div>
                 <div class="formdivs">
                     <label for="text">Wat doe je als je je verveelt?</label>
-                    <input type="text" name="verveelt" class="inputstuff"/>
-                    <span class="error">* <?php echo $nameErr;?></span>
+                    <input type="text" name="verveelt" class="inputstuff" value="<?php echo $verveelt;?>"/>
+                    <span class="error">* <?php echo $verveeltErr;?></span>
                 </div>
                 <div class="formdivs">
                     <label for="text">Met welk speelgoed speelde je als kind het meest?</label>
-                    <input type="text" name="speelgoed" class="inputstuff"/>
-                    <span class="error">* <?php echo $nameErr;?></span>
+                    <input type="text" name="speelgoed" class="inputstuff" value="<?php echo $speelgoed;?>"/>
+                    <span class="error">* <?php echo $speelgoedErr;?></span>
                 </div>
                 <div class="formdivs">
                     <label for="text">Bij welke docent spijbel je het liefst?</label>
-                    <input type="text" name="docent" class="inputstuff"/>
-                    <span class="error">* <?php echo $nameErr;?></span>
+                    <input type="text" name="spijbel" class="inputstuff" value="<?php echo $spijbel;?>"/>
+                    <span class="error">* <?php echo $spijbelErr;?></span>
                 </div>
                 <div class="formdivs">
                     <label for="text">Als je € 100.000,- had, wat zou je dan kopen?</label>
-                    <input type="text" name="geld" class="inputstuff"/>
-                    <span class="error">* <?php echo $nameErr;?></span>
+                    <input type="text" name="geld" class="inputstuff" value="<?php echo $geld;?>"/>
+                    <span class="error">* <?php echo $geldErr;?></span>
                 </div>
                 <div class="formdivs">
                     <label for="text">Wat is je favoriete bezigheid?</label>
-                    <input type="text" name="bezigheid" class="inputstuff"/>
-                    <span class="error">* <?php echo $nameErr;?></span>
+                    <input type="text" name="bezigheid" class="inputstuff" value="<?php echo $bezigheid;?>"/>
+                    <span class="error">* <?php echo $bezigheidErr;?></span>
                 </div>
                 <div class="submitdiv">
                     <input type="submit" value="Submit" class="submitbtn">
                 </div>
             </form>
+            </div>
         </div>
     </div>
 
@@ -76,6 +219,7 @@
     </div>
 
 </div>
+
 
 </body>
 </html>
